210e405b79b6d2859d67f55b4f97ef977d6e2852
haoHu <jackeysun1818@gmail.com>
2014-07-31 15:24:12 +0800

    update code for test data

--- test/data/app.js
+++ test/data/app.js
@@ -1,6 +1,32 @@
 (function () {
 	IX.ns("Test");
-	Test.SiteData = {};
-	Test.LoginUser = {};
-	Test.Roles = [];
+	Test.SiteData = {
+		groupName : "豆捞坊",
+		groupID : "5",
+		logo : "",
+		tel : ""
+	};
+	Test.LoginUser = {
+		role : [1, 2],
+		accountID : "87",
+		accountStatus : "1",
+		action : "1",
+		actionTime: "20140409173948",
+		createTime: "20120730085044",
+		groupID : "5",
+		loginName : "doulaofang-temp1",
+		shopID: "759821",
+		shopName: "豆捞坊(南京西路店)",
+		userEmail: "dongjiubo@11.com",
+		userMobile: "13122222222",
+		userName: "董九博",
+		userRemark: "董九博店小二"
+	};
+	Test.Roles = [
+		{id : 1, name : "店长"},
+		{id : 2, name : "财务"},
+		{id : 3, name : "区域经理"},
+		{id : 4, name : "集团经理"},
+		{id : 5, name : "系统管理员"}
+	];
 })();
